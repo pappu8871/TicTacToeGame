b2849a6ed31ffd80b59c049f395439a3fd71f8dc
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 18:48:00 +0530

    Added:TicTacToeUC3.java

--- Game.java
+++ Game.java
@@ -1,19 +1,42 @@
 package com.game;
 
-import java.lang.reflect.Array;
+import java.util.Scanner;
 
-public class TicTacGameUC2 {
-	static Array[] board;
+public class TicTacGameUC3 {
+
+	static String turn;
+	static String[] board;
 	static void printBoard() {
-		
-	}
-	public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	board = new Array[9];
+		System.out.println("|---|---|---|");
+		System.out.println("| " + board[0] + " | "
+	                           + board[1] + " | " + board[2]
+	                           + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | "
+	                           + board[4] + " | " + board[5]
+	                           + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | "
+	                           + board[7] + " | " + board[8]
+	                           + " |");
+		System.out.println("|---|---|---|");
+		}
 	
-	System.out.println();
-	printBoard();
-	System.out.println("player1 will play . Enter a slot number to place X in:");
-	}
-	}
+		public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    board = new String[9];
 
+//	   String winner = null;
+//	    for (int a = 0; a < 9; a++) {
+//	        board[a] = String.valueOf(a + 1);
+//	    }
+	   
+	   System.out.println();
+	   printBoard();
+	   System.out.println();
+	   System.out.println("Play with two player");
+	   System.out.println();
+	   System.out.println(
+	         "player 1st will play . Enter a slot number to place X in:");
+	   }
+	   }
