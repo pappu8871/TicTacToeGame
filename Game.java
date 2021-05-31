96973ae871346c1ae08eaab12db95d09032762c0
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 18:51:11 +0530

    Added:TicTacToeUc4>java

--- Game.java
+++ Game.java
@@ -2,41 +2,44 @@ package com.game;
 
 import java.util.Scanner;
 
-public class TicTacGameUC3 {
+public class TicTacGameUC4 {
 
 	static String turn;
 	static String[] board;
+
 	static void printBoard() {
 		System.out.println("|---|---|---|");
 		System.out.println("| " + board[0] + " | "
-	                           + board[1] + " | " + board[2]
-	                           + " |");
+				+ board[1] + " | " + board[2]
+						+ " |");
 		System.out.println("|-----------|");
 		System.out.println("| " + board[3] + " | "
-	                           + board[4] + " | " + board[5]
-	                           + " |");
+				+ board[4] + " | " + board[5]
+						+ " |");
 		System.out.println("|-----------|");
 		System.out.println("| " + board[6] + " | "
-	                           + board[7] + " | " + board[8]
-	                           + " |");
+				+ board[7] + " | " + board[8]
+						+ " |");
 		System.out.println("|---|---|---|");
-		}
+	} 
 	
-		public static void main(String[] args) {
-	    Scanner in = new Scanner(System.in);
-	    board = new String[9];
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		board = new String[9];
+		//turn = "X";
+		// String winner = null;
 
-//	   String winner = null;
-//	    for (int a = 0; a < 9; a++) {
-//	        board[a] = String.valueOf(a + 1);
-//	    }
-	   
-	   System.out.println();
-	   printBoard();
-	   System.out.println();
-	   System.out.println("Play with two player");
-	   System.out.println();
-	   System.out.println(
-	         "player 1st will play . Enter a slot number to place X in:");
-	   }
-	   }
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf(a + 1);
+		}
+
+		System.out.println();
+		printBoard();
+		System.out.println();
+		System.out.println("Play with two player");
+		System.out.println(
+				"player 1st will play . Enter a slot number to place X in:");
+	}
+}
+
+
