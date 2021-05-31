7fc7fb992b1d31e1dd1cd0915ee4671afe3d9d3e
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 23:01:12 +0530

    Added:TicTacToeUC9.java

--- Game.java
+++ Game.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class TictacGameUC8 {
+public class TicTacGameUC9 {
 
 	static String[] board;
 	static String turn;
@@ -12,8 +12,8 @@ public class TictacGameUC8 {
 	// CheckWinner method will 
 	// decide the combination 
 	static String checkWinner() {
-		for (int a = 0; a < 8; a++) {
-			String line = null;
+	for (int a = 0; a < 8; a++) {
+		String line = null;
 
 		switch (a) {
 		case 0:
@@ -40,30 +40,29 @@ public class TictacGameUC8 {
 		case 7:
 			line = board[2] + board[4] + board[6];
 			break;
-			}
+			}	            
 			//For X winner
-		if (line.equals("XXX")) {
+			if (line.equals("XXX")) {
 				return "X";
-		}
+			}
 
-		// For O winner
-		else if (line.equals("OOO")) {
+			// For O winner
+			else if (line.equals("OOO")) {
 				return "O";
-		}
-		}
-
+			}
+			}
+	
 		for (int a = 0; a < 9; a++) {
 			if (Arrays.asList(board).contains(String.valueOf(a + 1))) {
-				break;
+			break;
 			}
 			else if (a == 8) {
 				return "draw";
 			}
-		}
+			}
 
 		// To enter the X Or O at the exact place on board.
-		System.out.println(
-				turn + "'s turn; enter a slot number to place "
+		System.out.println(turn + "'s turn; enter a slot number to place "
 						+ turn + " in:");
 		return null;
 		}
@@ -101,23 +100,25 @@ public class TictacGameUC8 {
 
 		while (winner == null) {
 			int numInput;
+			
 			try {
 				numInput = in.nextInt();
 				if (!(numInput > 0 && numInput <= 9)) {
-					System.out.println("Invalid input; re-enter slot number:");
-					continue;
+				System.out.println("Invalid input; re-enter slot number:");
+				continue;
 			}
 			}
 			catch (InputMismatchException e) {
-			System.out.println("Invalid input; re-enter slot number:");
+				System.out.println("Invalid input; re-enter slot number:");
 				continue;
 			}
 
-			//check board is empty then take user value and turn in
-            //Box is pre taken input then show 
-            //This game has two player x and O.
-			if (board[numInput - 1].equals(String.valueOf(numInput))) {
+			// This game has two player x and O.
+			// Here is the logic to decide the turn.
+			if (board[numInput - 1].equals(
+					String.valueOf(numInput))) {
 				board[numInput - 1] = turn;
+
 				if (turn.equals("X")) {
 					turn = "O";
 				} else {
@@ -126,10 +127,11 @@ public class TictacGameUC8 {
 
 				printBoard();
 				winner = checkWinner();
+			} else {
+				System.out.println(
+						"Slot already taken; re-enter slot number:");
 			}
-			else {
-				System.out.println("Slot already taken; re-enter slot number:");
 			}
-		}
-		}
-		}
+			}
+			}
+
